b75b7c4efe338165c14d8d73acc77cf699481fa0
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2022-12-01 10:03:14 +0100

    some help

--- Util/config.js
+++ Util/config.js
@@ -1,19 +1,19 @@
 const config = {
-  width: 500,
+  width: 500, //gif width
 
-  height: 500,
+  height: 500, //gif height
 
-  frames: 10,
+  frames: 10, //number of gif frames
 
-  delay: 10,
+  delay: 10, //delay between frames in ms
 
-  quality: 10,
+  quality: 10, //gif quality
 
-  repeat: -1,
+  repeat: -1, //gif repeat 0 = forever , -1 = no repeat, 0> = repeat times
 
-  transparent: 0x00000000,
+  transparent: 0x00000000, //gif transparent color
 
-  pixelFill: 100,
+  pixelFill: 100, //pixel fill size x*y=pixelFill ^2
 
   path: "gifs/colors.gif",
 };
